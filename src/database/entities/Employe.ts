834ff2988bc4cd6889a39138608a1706e5812b78
vanande <vkhatchatrian1@myges.fr>
2024-04-27 16:15:54 +0200

    db add spectateur

--- src/database/entities/Employe.ts
+++ src/database/entities/Employe.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity({ name: "EMPLOYE" })
+@Entity({ name: "Employe" })
 export class Employe {
     @PrimaryGeneratedColumn()
     idEmploye: number
