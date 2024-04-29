d8f48cfa178b5f6199c694643fa7552ee2225ab4
vanande <vkhatchatrian1@myges.fr>
2024-04-30 00:05:35 +0200

    remove databaseS

--- dist/routes/hello.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloRoute = void 0;
-const helloRoute = (app) => {
-    app.get('/', (req, res) => {
-        res.send('Hello World!');
-    });
-};
-exports.helloRoute = helloRoute;
