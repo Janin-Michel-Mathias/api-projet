b98e1d4a5376b24d295be858b215dbf0df1f6c36
vanande <vkhatchatrian1@myges.fr>
2024-05-02 22:02:38 +0200

    feat/patch route for salle

--- src/handlers/validators/salle-validation.ts
+++ src/handlers/validators/salle-validation.ts
@@ -23,3 +23,24 @@ export const salleValidation = Joi.object<SalleRequest>({
         .default(false),
     etat: Joi.string()
 }).options({ abortEarly: false });
+
+
+export interface UpdateSalleRequest {
+    id: number
+    nom?: string,
+    description?: string,
+    images?: string,
+    capacite?: number,
+    accesHandicap?: boolean,
+    etat?: string 
+}
+
+export const updateSalleValidation = Joi.object<UpdateSalleRequest> ({
+    id: Joi.number().required(),
+    nom: Joi.string(),
+    description: Joi.string().min(2),
+    images: Joi.string(),
+    capacite: Joi.number().min(15).max(30),
+    accesHandicap: Joi.bool(),
+    etat: Joi.string().valid("Prêt", "En maintenance", "Occupé", "Fermé")
+}).options({ abortEarly: false });
--- src/routes/salle.ts
+++ src/routes/salle.ts
@@ -1,7 +1,7 @@
 import {Express, Request, Response} from 'express';
 import { AppDataSource } from '../database/database';
 import { Salle } from '../database/entities/Salle';
-import { salleValidation } from '../handlers/validators/salle-validation';
+import { salleValidation, updateSalleValidation } from '../handlers/validators/salle-validation';
 import { generateValidationErrorMessage } from '../handlers/validators/generate-validation-message';
 
 export const salleRoute = (app: Express):void => {
@@ -32,7 +32,26 @@ export const salleRoute = (app: Express):void => {
             console.log(error);
             res.status(500).send({ error : "Internal Error"})
         }
-
     });
+
+    app.patch('salle/:id', async(req: Request, res: Response) => {
+        const validate = updateSalleValidation.validate({...req.params, ...req.body})
+        
+        if (validate.error){
+            res.status(400).send(generateValidationErrorMessage(validate.error.details))
+            return
+        }
+        
+        const request = validate.value
+        const repo = AppDataSource.getRepository(Salle)
+
+        try {
+            const salleUpdated = await repo.save(request)
+            res.status(200).send(salleUpdated)
+        } catch (error) {
+            res.status(500).send({error: "Internal Error"})
+        }
+
+    })
 
 }
