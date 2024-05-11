a4daa6ade386488d8b1fdad152647c7e1bf82127
Baguette42 <87232192+Baguette42@users.noreply.github.com>
2024-05-11 22:23:45 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import * as routes from './routes';
 import { AppDataSource } from './database/database';
 import 'dotenv/config';
+import { collectDefaultMetrics, register } from 'prom-client';
 
 const main = async() => {
   const app = express();
+  collectDefaultMetrics();
   app.use(express.json())
+
 
   AppDataSource.initialize()
       .then(() => {
@@ -18,6 +21,14 @@ const main = async() => {
       (routes as any)[routeName](app);
   });
 
+  app.get('/metrics', async (_req, res) => {
+      try {
+          res.set('Content-Type', register.contentType);
+          res.end(await register.metrics());
+      } catch (err) {
+          res.status(500).end(err);
+      }
+  });
 
   app.listen(3000, () => {
     console.log('Server is listening on port 3000');
